a18ac5cecf803e53eb4ddc6e1651342fd75ae304
Luiz <luizvnx@gmail.com>
2023-06-14 17:38:27 -0300

    Commit Inicial

---
+++ e-commerce/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+import { Header } from './components/Header/Header';
+import { About } from './components/About/About';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Header/>
+      <About/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ e-commerce/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'
+import Logo from '../../assets/img.jpg';
+
+
+export const Header = () => {
+    return (
+    
+        <div className='header'>
+            <a href="*">
+                <img src={Logo} alt="Logo"/>
+            </a>
+            <h1>NavBar</h1>
+            <hr/>
+        </div>
+        
+    );
+};
+
